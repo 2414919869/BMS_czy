f4c9f38c8c8cb9cb4cad42d80afc62a117206ae6
2414919869 <2414919869@qq.com>
2024-06-05 11:00:22 +0800

    4、测试以json数组格式返回数据

--- project/BMS/src/main/java/com/czy/bms/controller/TestController.java
+++ project/BMS/src/main/java/com/czy/bms/controller/TestController.java
@@ -2,6 +2,8 @@ package com.czy.bms.controller;
 
 
 import com.czy.bms.response.CommonResp;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,23 @@ public class TestController {
         return new CommonResp().success()
                 .data(new Date());
     }
+
+    @GetMapping("/json")
+    public CommonResp testJson(){
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("name","caizeyu");
+        jsonObject1.put("status","admin");
+        jsonObject1.put("time","20240605");
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("name","Tom");
+        jsonObject2.put("status","user");
+        jsonObject2.put("time","20240604");
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(jsonObject1);
+        jsonArray.add(jsonObject2);
+        return new CommonResp().success()
+                .data(jsonArray);
+    }
+
 
 }
